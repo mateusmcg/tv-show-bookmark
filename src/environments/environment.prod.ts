35554409d85c4678acaa21722fcb302f535e2b9c
Mateus Cerqueira <mateus.mcg@gmail.com>
2018-09-02 22:07:13 -0300

    Updating env.prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,6 +9,8 @@ export const environment = {
     messagingSenderId: '883297663795'
   },
   traktTvBaseUrl: 'https://api.trakt.tv',
+  traktTvClientId: 'b0193b6297b2e397aeef7b832872e50752eae47b10c809f8dec7b98f34aafab1',
+  traktTvApiVersion: 2,
   tmdbBaseUrl: 'https://api.themoviedb.org/3/',
   tmdbApiKey: '6c4c055b1cbd17bdfb3c8991d519ba91'
 };
